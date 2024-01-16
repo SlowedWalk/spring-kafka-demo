c6d3692a2bcb74a4f331a9930f68296ab568bb99
SlowedWalk <idetoratojo1@gmail.com>
2024-01-16 04:44:16 +0100

    Init

---
+++ src/test/java/tech/hidetora/kafkatutorial/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hidetora.kafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
